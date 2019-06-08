0a7cd16495be4566bd83515cfe4dac9fd291159c
Nick Guerra <nickg@maliciousgames.com>
2019-06-08 09:19:53 -0500

    troubleshooting heroku logs

--- server.js
+++ server.js
@@ -36,7 +36,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-//app.listen(port, () => console.log(`Server started on port ${port}`));
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
