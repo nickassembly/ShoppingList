8c60680560d22a4b2c04b4e26b678eb48a94f265
Nick Guerra <nickg@maliciousgames.com>
2019-06-08 09:15:31 -0500

    change port

--- server.js
+++ server.js
@@ -36,4 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+//app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
